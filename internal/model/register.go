19370377db6394536f7fdbbdf8da5e0c8eba651f
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-03 20:24:58 +0530

    add validator

--- internal/model/register.go
+++ internal/model/register.go
@@ -1,7 +1,7 @@
 package model
 
 type RegisterRequest struct {
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
+	Name     string `json:"name" validate:"required,min=2,max=100"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=6,max=72"`
 }
