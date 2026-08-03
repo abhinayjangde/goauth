1d9ff0dd8bfc7c7ed16c34b35b53f9281d5f5be2
Abhinay Jangde <abhinayjangde@gmail.com>
2026-08-03 20:51:29 +0530

    add error response

--- internal/validator/validator.go
+++ internal/validator/validator.go
@@ -1,7 +1,29 @@
 package validator
 
-import "github.com/go-playground/validator/v10"
+import (
+	"reflect"
+	"strings"
+
+	"github.com/go-playground/validator/v10"
+)
 
 var Validate = validator.New(
 	validator.WithRequiredStructEnabled(),
 )
+
+func init() {
+	Validate.RegisterTagNameFunc(func(fld reflect.StructField) string {
+
+		name := strings.SplitN(
+			fld.Tag.Get("json"),
+			",",
+			2,
+		)[0]
+
+		if name == "-" {
+			return ""
+		}
+
+		return name
+	})
+}
